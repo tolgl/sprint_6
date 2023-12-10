ad0244d98ce117a596819d4de51b135b237177d5
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-12-10 23:54:14 +0300

    изменены импользуемые методы для теста

--- tests/test_make_order.py
+++ tests/test_make_order.py
@@ -1,24 +1,20 @@
-from selenium import webdriver
-from pages.order_page import OrderPage
+from pages.main_page import MainPageHelper
+from pages.order_page import OrderPageHelper
 import allure
 
 
 class TestMakeOrder:
-    driver = None
-
-    @classmethod
-    def setup_class(cls):
-        cls.driver = webdriver.Firefox()
-        cls.driver.fullscreen_window()
 
     @allure.title('Проверка оформления заказа по кнопке "Заказать" вверху страницы')
-    def test_make_order_on_header(self):
-        self.driver.get('https://qa-scooter.praktikum-services.ru/')
-        # объект класса страницы оформления заказа
-        order_page = OrderPage(self.driver)
+    def test_make_order_on_header(self, driver):
+        # объект класса главной страницы
+        main_page = MainPageHelper(driver)
+        # открытие страницы
+        main_page.go_to_page()
         # метод нажатия на кнопку "Заказать" в хедере
-        order_page.click_on_button_order_on_header()
+        main_page.click_on_button_order_on_header()
         # метод заполнения формы оформления заказа
+        order_page = OrderPageHelper(driver)
         order_page.filling_order_form('Тест', 'Тестов', 'ул. Тестовая, 15', '89999999999')
         # метод получения текста на форме успешного оформления заказа
         actual_result = order_page.get_text_successful_order()
@@ -26,19 +22,17 @@ class TestMakeOrder:
         assert 'Заказ оформлен' in actual_result
 
     @allure.title('Проверка оформления заказа по кнопке "Заказать" внизу страницы')
-    def test_make_order_on_footer(self):
-        self.driver.get('https://qa-scooter.praktikum-services.ru/')
-        # объект класса страницы оформления заказа
-        order_page = OrderPage(self.driver)
+    def test_make_order_on_footer(self, driver):
+        # объект класса главной страницы
+        main_page = MainPageHelper(driver)
+        # открытие страницы
+        main_page.go_to_page()
         # метод нажатия на кнопку "Заказать" внизу страницы
-        order_page.click_on_button_order_on_footer()
+        main_page.click_on_button_order_on_footer()
         # метод заполнения формы оформления заказа
+        order_page = OrderPageHelper(driver)
         order_page.filling_order_form('Тест', 'Тестов', 'ул. Тестовая, 15', '89999999999')
         # метод получения текста на форме успешного оформления заказа
         actual_result = order_page.get_text_successful_order()
 
         assert 'Заказ оформлен' in actual_result
-
-    @classmethod
-    def teardown_class(cls):
-        cls.driver.quit()
